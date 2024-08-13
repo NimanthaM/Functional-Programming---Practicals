6adafd016e2d7864f0c43f79cb1a269d00fa7ac2
NimanthaM <140166591+NimanthaM@users.noreply.github.com>
2024-08-13 08:40:53 +0530

    Update labsheet8.scala

--- labsheet8.scala
+++ labsheet8.scala
@@ -1,22 +1,65 @@
-object CaesarCipher {
-
-  def encrypt(text: String, shift: Int): String = {
-    text.map {
-      case c if c.isUpper =>
-        ((c - 'A' + shift) % 26 + 'A').toChar
-      case c if c.isLower =>
-        ((c - 'a' + shift) % 26 + 'a').toChar
-      case c => c
+import scala.io.StdIn
+//Question 01
+def encrypt(text: String, shift: Int): String = {
+  text.map { c =>
+    if (c.isUpper) {
+      ((c - 'A' + shift) % 26 + 'A').toChar
+    } else if (c.isLower) {
+      ((c - 'a' + shift) % 26 + 'a').toChar
+    } else {
+      c
     }
   }
+}
 
-  def decrypt(text: String, shift: Int): String = {
-    text.map {
-      case c if c.isUpper =>
-        ((c - 'A' - shift + 26) % 26 + 'A').toChar
-      case c if c.isLower =>
-        ((c - 'a' - shift + 26) % 26 + 'a').toChar
-      case c => c
+// Decryption
+def decrypt(text: String, shift: Int): String = {
+  text.map { c =>
+    if (c.isUpper) {
+      ((c - 'A' - shift + 26) % 26 + 'A').toChar
+    } else if (c.isLower) {
+      ((c - 'a' - shift + 26) % 26 + 'a').toChar
+    } else {
+      c
     }
   }
+}
+
+def cipher(processFunction: (String, Int) => String, text: String, shift: Int): String = {
+  processFunction(text, shift)
+}
+
+
+//Question 02
+
+val multipleOfThree: Int => Boolean = (x: Int) => x % 3 == 0
+val multipleOfFive: Int => Boolean = (x: Int) => x % 5 == 0
+val multipleOfBoth: Int => Boolean = (x: Int) => x % 3 == 0 && x % 5 == 0
+
+def categorizeNumber(num: Int): String = num match {
+    case n if multipleOfBoth(n) => "Multiple of Both Three and Five"
+    case n if multipleOfThree(n) => "Multiple of Three"
+    case n if multipleOfFive(n) => "Multiple of Five"
+    case _ => "Not a Multiple of Three or Five"
+}
+
+@main def main (): Unit = {
+
+//q1
+printf("Question 01\n\n")
+val text = "Hello, World!"
+val shift = 2
+
+val encryptedText = cipher(encrypt, text, shift)
+println(s"Encrypted: $encryptedText")
+
+val decryptedText = cipher(decrypt, encryptedText, shift)
+println(s"Decrypted: $decryptedText")
+
+//q2
+printf("\n\nQuestion 02\n\n")
+println("Enter an integer:")
+val input = StdIn.readInt()
+
+println(categorizeNumber(input))
 }
